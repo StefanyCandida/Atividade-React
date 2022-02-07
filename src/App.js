dc4226f0f3850b491831093956c0b9e9154d7907
StefanyCandida <stefany.candida@gmail.com>
2022-02-07 12:16:21 -0300

    inserindo projeto1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+  function Projeto{
+    return(
+      <main>
+        <h1> Primeiro projeto React function Component</h1>
+        <p> Foguete nãp dá ré</p>
+    </main>
+    );
+  }
+export default Projeto
+
